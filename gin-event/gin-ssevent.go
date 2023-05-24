4437f3bf6b4a187a8ad6305161e44c6cbe6058e0
bingbing.fu <bingbing.fu@fanli.com>
2023-05-24 19:10:31 +0800

    up

--- gin-event/gin-ssevent.go
+++ gin-event/gin-ssevent.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -14,37 +12,36 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/stream", func(c *gin.Context) {
-		// 设置响应头，指定为事件流格式
 		c.Header("Content-Type", "text/event-stream")
 		c.Header("Cache-Control", "no-cache")
 		c.Header("Connection", "keep-alive")
 
-		// 启动事件流
-		flusher, _ := c.Writer.(http.Flusher)
-		if flusher == nil {
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
+		// 创建一个只写的响应流
+		responseWriter := c.Writer
+		flusher, _ := responseWriter.(http.Flusher)
 
-		eventStream := make(chan string)
-
+		// 启动 goroutine 处理流式响应
 		go func() {
-			writer := bufio.NewWriter(c.Writer)
+			defer c.Done()
 
-			for {
-				event := <-eventStream
-				fmt.Fprintf(writer, "data: %s\n\n", event)
-				writer.Flush()
+			// 模拟每秒发送一个事件
+			for i := 0; i < 5; i++ {
+				// 构造事件数据
+				event := fmt.Sprintf("Event %d", i+1)
+
+				// 写入事件数据到响应流
+				fmt.Fprintf(responseWriter, "data: %s\n\n", event)
+
+				// 刷新响应流
 				flusher.Flush()
+
+				// 模拟延迟 1 秒
+				time.Sleep(time.Second)
 			}
 		}()
 
-		// 模拟产生事件并写入事件流
-		for i := 0; i < 5; i++ {
-			time.Sleep(time.Second)
-			event := "Event " + strconv.Itoa(i+1)
-			eventStream <- event
-		}
+		// 等待上下文结束
+		<-c.Done()
 	})
 
 	router.Run(":8080")
